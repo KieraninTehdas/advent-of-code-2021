b821dbf38f8af54ef61ecd9167c706ff7ab25878
Kieran Porter <kieranintehdas@gmail.com>
2021-12-01 22:39:35 +0000

    Find n sliding window increases for part 2

--- 1/sonar_scanner.py
+++ 1/sonar_scanner.py
@@ -1,12 +1,12 @@
 import sys
+from collections import deque
 
-if __name__ == "__main__":
-    input_filename = sys.argv[1]
 
+def calculate_simple_reading_increase_frequency(filename: str):
     previous_reading = None
     number_of_increased_readings = 0
 
-    for sonar_reading in open(input_filename, "r"):
+    for sonar_reading in open(filename, "r"):
         sonar_reading = int(sonar_reading)
 
         if previous_reading and sonar_reading > previous_reading:
@@ -14,4 +14,31 @@ if __name__ == "__main__":
 
         previous_reading = sonar_reading
 
-    print(number_of_increased_readings)
+    return number_of_increased_readings
+
+
+def calculate_sliding_window_increase_frequency(filename: str, window_length: int = 3):
+    number_of_increased_readings = 0
+    previous_window_sum = None
+    window = deque([], maxlen=window_length)
+
+    for sonar_reading in open(filename, "r"):
+        window.append(int(sonar_reading))
+
+        if len(window) == window_length:
+            window_sum = sum(window)
+
+            if previous_window_sum and window_sum > previous_window_sum:
+                number_of_increased_readings += 1
+
+            previous_window_sum = window_sum
+
+    return number_of_increased_readings
+
+
+if __name__ == "__main__":
+    input_filename = sys.argv[1]
+
+    result = calculate_sliding_window_increase_frequency(input_filename)
+
+    print(result)
