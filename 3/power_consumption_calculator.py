5a25f4c8726b2bfa413da9f598ac57d32c1b826b
Kieran Porter <kieranintehdas@gmail.com>
2021-12-04 00:02:51 +0000

    Df

--- 3/power_consumption_calculator.py
+++ 3/power_consumption_calculator.py
@@ -1,7 +1,9 @@
 import sys
 from collections import defaultdict
 from operator import itemgetter
-from typing import Callable
+from typing import Callable, List
+from itertools import count
+from statistics import multimode
 
 
 def calculate_gamma_rate(bit_value_counts: dict) -> int:
@@ -21,18 +23,81 @@ def _calculate_rate(bit_value_counts: dict, count_evaluator: Callable) -> int:
     return int(binary_value, 2)
 
 
+def calculate_power_consumption(bit_value_counts: dict) -> int:
+    return calculate_gamma_rate(bit_value_counts) * calculate_epsilon_rate(
+        bit_value_counts
+    )
+
+
+def _calculate_oxygen_rating_criteria(bit_values: List[int]) -> int:
+    most_common_bit_values = multimode(bit_values)
+
+    if len(most_common_bit_values) > 1:
+        criteria = 1
+    else:
+        criteria = most_common_bit_values[0]
+
+    return criteria
+
+
+def calculate_oxygen_generator_rating(diagnostic_values: List[str]) -> int:
+    return _calculate_rating(diagnostic_values, _calculate_oxygen_rating_criteria)
+
+
+def _calculate_co2_rating_criteria(bit_values: List[int]) -> int:
+    bit_value_frequencies = defaultdict(lambda: 0)
+
+    for value in bit_values:
+        bit_value_frequencies[value] += 1
+
+    if bit_value_frequencies[0] == bit_value_frequencies[1]:
+        criteria = 0
+    else:
+        criteria = min(bit_value_frequencies, key=bit_value_frequencies.get)
+
+    return criteria
+
+
+def calculate_co2_scrubber_rating(diagnostic_values: List[str]) -> int:
+    return _calculate_rating(diagnostic_values, _calculate_co2_rating_criteria)
+
+
+def _calculate_rating(reported_values: List[str], criteria_calculator: Callable) -> int:
+    matching_values = reported_values
+
+    for i in count():
+        if len(matching_values) == 1:
+            return int(matching_values[0], 2)
+
+        try:
+            bit_values_at_position = [int(line[i]) for line in matching_values]
+        except IndexError:
+            raise RuntimeError("Failed to find a single matching line")
+
+        criteria = criteria_calculator(bit_values_at_position)
+
+        matching_values = [line for line in matching_values if int(line[i]) == criteria]
+
+
+def calculate_life_support_rating(diagnostic_values: List[str]) -> int:
+    return calculate_co2_scrubber_rating(
+        diagnostic_values
+    ) * calculate_oxygen_generator_rating(diagnostic_values)
+
+
 if __name__ == "__main__":
     filename = sys.argv[1]
 
     bit_value_counts_by_position = defaultdict(lambda: defaultdict(lambda: 0))
 
-    for line in open(filename, "r"):
-        line = line.strip()
-        for i in range(0, len(line)):
-            bit_value_counts_by_position[i][int(line[i])] += 1
+    diagnostic_values = [line.strip() for line in open(filename, "r")]
 
-    power_consumption = calculate_epsilon_rate(
-        bit_value_counts_by_position
-    ) * calculate_gamma_rate(bit_value_counts_by_position)
+    for diagnostic_value in diagnostic_values:
+        for i in range(0, len(diagnostic_value)):
+            bit_value_counts_by_position[i][int(diagnostic_value[i])] += 1
 
-    print(power_consumption)
+    print(
+        f"Power consumption is {calculate_power_consumption(bit_value_counts_by_position)}"
+    )
+
+    print(f"Life support rating is {calculate_life_support_rating(diagnostic_values)}")
