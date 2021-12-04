50b048a069dec4b5c47c1e0d81b8a249a95772be
Kieran Porter <kieranintehdas@gmail.com>
2021-12-04 20:19:22 +0000

    Working bingo

--- 4/bingo.py
+++ 4/bingo.py
@@ -1,22 +1,67 @@
 import sys
 from typing import List
 from pprint import pprint
+import itertools
 
+
+class BoardElement:
+    def __init__(self, number: int):
+        self.number = number
+        self.is_marked = False
+
+    def mark(self):
+        self.is_marked = True
 
 class BingoBoard:
     def __init__(self, board_lines: List[List[int]]):
-        self.board_lines = board_lines
-        self.dimensions = (len(board_lines[0]), len(board_lines))
+        self.rows = board_lines
+        self.n_rows = len(board_lines)
+        self.n_columns = len(board_lines[0])
+        self.marked_rows = [[False for _ in range(0, self.n_columns)] for _ in range(0, self.n_rows)]
+        pprint(self.marked_rows)
 
-    def contains(self, number: int):
-        for line in self.board_lines:
-            if number in line:
-                return True
-            else:
+
+    def _find_index_of(self, number: int):
+        for row_number, row in enumerate(self.rows):
+            try:
+                return (row_number, row.index(number))
+            except ValueError:
                 continue
-        
-        return False
 
+        return None
+
+    def mark_number_if_present(self, number: int):
+        index_of_number = self._find_index_of(number)
+
+        if not index_of_number:
+            return
+
+        self.marked_rows[index_of_number[0]][index_of_number[1]] = True
+
+    def is_complete(self):
+       return self._has_complete_row() or self._has_complete_column()
+
+
+
+    def _has_complete_row(self):
+        return self._all_marked(self.marked_rows)
+        
+    def _has_complete_column(self):
+        marked_columns = [[] for _ in range(0, self.n_columns)]
+        for row in self.marked_rows:
+            for column_number, marked_value in enumerate(row):
+                marked_columns[column_number].append(marked_value)
+
+        return self._all_marked(marked_columns)
+
+
+    @staticmethod
+    def _all_marked(marked_values: List[List[bool]]) -> bool:
+        for row in marked_values:
+           if row[0] is True and len(set(row)) == 1:
+               return True
+
+        return False
 
 
 def parse_boards(filename: str) -> List[BingoBoard]:
@@ -47,4 +92,20 @@ if __name__ == "__main__":
 
     boards = parse_boards(boards_file)
 
-    [print(board.contains(1)) for board in boards]
+    numbers = [13, 2, 9, 10, 12, 15]
+
+    winner = None
+
+    for number in numbers:
+        if winner:
+            pprint("We have a winner!")
+            pprint(winner.rows)
+            break
+
+        print(f"Number is: {number}")
+
+        for board in boards:
+            board.mark_number_if_present(number)
+            if board.is_complete():
+                winner = board
+                break
