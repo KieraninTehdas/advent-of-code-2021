e2e48d05fc72b7225c2005acd0ca308e32864942
Kieran Porter <kieranintehdas@gmail.com>
2021-12-08 23:30:07 +0000

    Day 6 tidied up a little

--- 6/lanternfish_simulator.py
+++ 6/lanternfish_simulator.py
@@ -1,8 +1,6 @@
 from __future__ import annotations
 import sys
-from pprint import pprint
 import itertools
-from typing import List
 
 DEFAULT_DAYS_TO_RUN = 80
 
@@ -41,34 +39,6 @@ def simulate_fish_growth(filename: str):
     print(f"There are now {len(fishes)} after {days_to_run} days")
 
 
-def convert_decimal_to_heximal(decimal: int) -> List[int]:
-    bits = []
-    quotient, remainder = divmod(decimal, 6)
-    quotient = decimal
-
-    while quotient != 0:
-        print(quotient)
-        quotient, remainder = divmod(quotient, 6)
-        bits.append(remainder)
-
-    bits.reverse()
-    return bits
-
-def calculate_n_fish_spawned(initial_timer: int, days_remaining: int) -> int:
-    remaining_days_after_first_spawn = days_remaining - initial_timer
-
-    return (remaining_days_after_first_spawn // 6) + 1
-
-def calculate_spawn_days_after_start(initial_timer: int, starting_day: int, ending_day: int) -> List[int]:
-    if initial_timer + starting_day > ending_day:
-        return []
-
-    n_fish_spawned = calculate_n_fish_spawned(initial_timer, ending_day - starting_day)
-
-    return [starting_day + initial_timer + (7 * days) for days in range(0, n_fish_spawned) if (starting_day + initial_timer + (7*days)) <= ending_day]
-
-
-
 if __name__ == "__main__":
     filename = sys.argv[1]
     try:
@@ -76,17 +46,26 @@ if __name__ == "__main__":
     except KeyError:
         days_to_run = DEFAULT_DAYS_TO_RUN
 
+    initial_fish_timers = [
+        int(timer.strip())
+        for timer in itertools.chain.from_iterable(
+            [line.split(",") for line in open(filename, "r")]
+        )
+    ]
+
     fish_timers = {timer: 0 for timer in range(0, 9)}
-    initial_fish_timers = [int(timer.strip()) for timer in itertools.chain.from_iterable([line.split(",") for line in open(filename, "r")])]
 
     for timer in initial_fish_timers:
         fish_timers[timer] += 1
 
     for _ in range(0, days_to_run):
-        fish_timers = {time_to_spawn - 1: n_fish for time_to_spawn, n_fish in fish_timers.items()}
+        fish_timers = {
+            time_to_spawn - 1: n_fish for time_to_spawn, n_fish in fish_timers.items()
+        }
         fish_timers[8] = fish_timers[-1]
         fish_timers[6] = fish_timers[6] + fish_timers[-1]
         del fish_timers[-1]
-                
 
-    pprint(sum(fish_timers.values()))
+    print(
+        f"After {days_to_run} days there are {sum(fish_timers.values())} lantern fish"
+    )
